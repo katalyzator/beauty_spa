f5332b6ad78d6c6fe1c9de7879a85b8b0ffea640
katalyzator <web.coder96@gmail.com>
2018-04-12 19:22:35 +0600

    added new model without link with view and template

--- main/admin.py
+++ main/admin.py
@@ -3,4 +3,14 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-# Register your models here.
+from main.models import Member
+
+
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ['full_name', 'profession']
+
+    class Meta:
+        model = Member
+
+
+admin.site.register(Member, MemberAdmin)
--- main/models.py
+++ main/models.py
@@ -2,5 +2,20 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils.encoding import smart_unicode
 
-# Create your models here.
+
+class Member(models.Model):
+    full_name = models.CharField(max_length=300, verbose_name='ФИО')
+    profession = models.CharField(max_length=255, verbose_name='Специализация')
+    description = models.TextField(max_length=1000, verbose_name='Описание')
+
+    timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+
+    class Meta:
+        verbose_name_plural = 'Спецалисты'
+        verbose_name = 'Специалиста'
+
+    def __unicode__(self):
+        return smart_unicode(self.full_name)
